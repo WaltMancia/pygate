e920f23c4ed54f3c5a1a872d92bb1c644f941d88
Walter Mancia <manciaw20@gmail.com>
2026-06-13 08:11:42 -0600

    fix(db): restore sqlalchemy session factory

---
+++ app/db/session.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import (
+    sessionmaker,
+)
+
+DATABASE_URL = (
+    "mysql+pymysql://root:root@localhost/pygate"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
